1369d92a8cc63fb51266d8f5fc72ebc0818faf35
CutNurhidayanti <cutnurhidayanti03@gmail.com>
2022-04-30 09:48:08 +0700

    user login dan perbaikan code model baru

--- resources/views/user/user-login.blade.php
+++ resources/views/user/user-login.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.login')
 
 @section('container')
-    
+<form action="/daftar" method="post" class="daftar">
+    @csrf
+    <div class="container justify-content-center">
+        <h1>Login</h1>
+        <form>
+            <div class="form-group">
+                <label>Email </label>
+                <input type="textarea" name="nim" class="form-control" placeholder="NIM">
+            </div>
+            <div class="form-group">
+                <label>Password </label>
+                <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
+            
+            <center><button type="submit" class="registerbtn" >Selanjutnya</button></center>
+
+            <div class="container-signin" style="text-align: center">
+                <p>Already have an account? <a href="login">Sign in</a>.</p>
+            </div>
+        </form>
+</form>
+    </form>
+
 @endsection

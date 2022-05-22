fee2e5d3ebcf6d77048bf2b7f04af2f9d322c7fc
CutNurhidayanti <cutnurhidayanti03@gmail.com>
2022-05-22 16:05:09 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\CalonKetua;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,6 +25,23 @@ class DatabaseSeeder extends Seeder
             'email' => 'anas@gmail.com',
             'Status' => False,
 
+        ]);
+
+        CalonKetua::create([
+            'image' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+            'nama' => 'Anas Naufal Al-Kiram',
+            'nim' => '2008107010065',
+            'visi' => 'babayy pilox',
+            'misi' => 'di catt',
+        ]);
+
+
+        CalonKetua::create([
+            'image' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+            'nama' => 'Angkasah',
+            'nim' => '2008107010017',
+            'visi' => 'babayy pilox',
+            'misi' => 'di catt',
         ]);
     }
 }
